19943fd9084e806bdb28f5fd92532624fa06026f
shashikant gupta <skantjio9140@gmail.com>
2023-11-28 01:08:07 +0530

    java program to find factorial

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; // Change this number to calculate factorial for a different value
+        
+        long factorial = calculateFactorial(number);
+        
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+    
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
